0b388aac7116e490f5d0ff271b48572c5407122d
Felix Salcher <dev.felix.salcher@gmail.com>
2024-04-10 19:49:03 +0200

    updated productseeder and configured phpstan to look into database folder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,8 +4,14 @@ namespace Database\Factories;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 
+/**
+ * @template TModel of Model
+ *
+ * @extends Factory<TModel>
+ */
 class ProductFactory extends Factory
 {
     protected $model = Product::class;
@@ -20,9 +26,9 @@ class ProductFactory extends Factory
             'updated_at' => Carbon::now(),
             'name' => $this->faker->name(),
             'description' => $this->faker->text(),
-            'price' => $this->faker->randomFloat(),
-            'images' => $this->faker->word(),
-            'quantity' => $this->faker->randomNumber(),
+            'price' => $this->faker->randomFloat(min: 10, max: 100),
+            'images' => $this->faker->imageUrl(),
+            'quantity' => $this->faker->randomNumber(2, true),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Felix Salcher',
+            'email' => 'felix.salcher@gmail.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true,
         ]);
+
+        Product::factory(20)->create();
     }
 }
