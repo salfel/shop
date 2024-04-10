91a3bdbd5ff2db6d39884c8037fbfc00ccf73a09
Felix Salcher <dev.felix.salcher@gmail.com>
2024-04-10 09:15:51 +0200

    added wireui

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './vendor/wireui/wireui/resources/**/*.blade.php',
+        './vendor/wireui/wireui/ts/**/*.ts',
+        './vendor/wireui/wireui/src/View/**/*.php',
     ],
 
     theme: {
