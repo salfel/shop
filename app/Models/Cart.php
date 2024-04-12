68051586d537a3f1eac0c2b3bf96a84c189c0be5
Felix Salcher <dev.felix.salcher@gmail.com>
2024-04-12 10:34:57 +0200

    added cart model and table

---
+++ app/Models/Cart.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Cart extends Model
+{
+    /**
+     * @return BelongsTo<User, Cart>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return BelongsToMany<Product>
+     */
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+}
---
+++ app/Models/CartProduct.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartProduct extends Model
+{
+    protected $fillable = [
+        'cart_id',
+        'product_id'
+    ];
+}
