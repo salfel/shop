75441312bef8a547e0f3e3021dde47b912bfa9f1
Felix Salcher <dev.felix.salcher@gmail.com>
2024-04-13 23:33:52 +0200

    when product already exists in cart, add 1 to the product in cart

--- resources/views/livewire/products/add-cart.blade.php
+++ resources/views/livewire/products/add-cart.blade.php
@@ -3,21 +3,32 @@
 use App\Models\CartProduct;
 use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use JetBrains\PhpStorm\NoReturn;
 use Livewire\Volt\Component;
 
 new class extends Component {
 
     public int $id;
 
-    public function addToCart(): void
+    #[NoReturn] public function addToCart(): void
     {
         $cart = Auth::user()->cart;
 
-        CartProduct::create([
-            'product_id' => $this->id,
-            'cart_id' => $cart->id,
-            'amount' => 1
-        ]);
+        $cartProduct = CartProduct::where('product_id', $this->id)->where('cart_id', $this->id)->first();
+
+        if ($cartProduct == null) {
+            Log::info('Creating new cart product');
+            CartProduct::create([
+                'product_id' => $this->id,
+                'cart_id' => $cart->id,
+                'amount' => 1
+            ]);
+        } else {
+            $cartProduct->update([
+                'amount' => $cartProduct->amount + 1
+            ]);
+        }
 
         $this->redirectRoute('cart');
     }
@@ -25,4 +36,4 @@ new class extends Component {
 
 ?>
 
-<x-button wire:click="addToCart" primary size="lg" label="Add to Cart" />
+<x-button wire:click="addToCart" primary size="lg" label="Add to Cart"/>
