e635acb01a1f3ad3bed1d7e12872f5d5b1c410af
Felix Salcher <dev.felix.salcher@gmail.com>
2024-04-14 12:52:22 +0200

    sync stripe products with database entries

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -20,4 +20,31 @@ class Product extends Model
     protected $casts = [
         'images' => 'array',
     ];
+
+    protected static function booted(): void
+    {
+        static::created(function (Product $product) {
+            $stripe = new \Stripe\StripeClient(config('cashier.secret'));
+
+            $stripeProduct = $stripe->products->create([
+                'name' => $product->name,
+                'default_price_data' => [
+                    'currency' => 'eur',
+                    'unit_amount' => $product->price * 100,
+                ],
+                'images' => $product->images,
+                'expand' => ['default_price'],
+            ]);
+
+            $product->update([
+                'stripe_id' => $stripeProduct->default_price->id,
+            ]);
+        });
+
+        static::deleted(function (Product $product) {
+            $stripe = new \Stripe\StripeClient(config('cashier.secret'));
+
+            $stripe->products->delete($product->stripe_id);
+        });
+    }
 }
